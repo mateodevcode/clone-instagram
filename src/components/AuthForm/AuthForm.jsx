48a84d959f551f021414458417e74c6acc4e53ca
Mateo Lizcano <bysteffler@gmail.com>
2023-12-31 14:53:32 +0100

    proyecto finalizado

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,61 +1,45 @@
-import {
-  Box,
-  VStack,
-  Image,
-  Flex,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 import Login from "./Login";
-import SignUp from "./SignUp";
+import Signup from "./Signup";
 import GoogleAuth from "./GoogleAuth";
 
+const AuthForm = () => {
+	const [isLogin, setIsLogin] = useState(true);
 
+	return (
+		<>
+			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
+				<VStack spacing={4}>
+					<Image src='/logo.png' h={24} cursor={"pointer"} alt='Instagram' />
 
+					{isLogin ? <Login /> : <Signup />}
 
+					{/* ---------------- OR -------------- */}
+					<Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+						<Box flex={2} h={"1px"} bg={"gray.400"} />
+						<Text mx={1} color={"white"}>
+							OR
+						</Text>
+						<Box flex={2} h={"1px"} bg={"gray.400"} />
+					</Flex>
 
-function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
- 
-  return (
-    <>
-      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-        <VStack spacing={4}>
-          <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+					<GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
+				</VStack>
+			</Box>
 
-        {isLogin ? <Login /> : <SignUp />}
-
-          {/* -----------------OR TEXT-------------- */}
-          <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            my={4}
-            gap={1}
-            w={"full"}
-          >
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
-            <Text margin={1} color={"white"}>
-              OR
-            </Text>
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
-          </Flex>
-          <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"}/>
-        </VStack>
-      </Box>
-      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
-        <Flex alignItems={"center"} justifyContent={"center"}>
-          <Box mx={2} fontSize={14}>
-
-            {isLogin ? "Don't have an account" : "Already have an account?"}
-          </Box>
-          <Box cursor={"pointer"} color={"blue.500"} onClick={() => setIsLogin(!isLogin ? true : false)}>
-
-            {isLogin ? "Sign Up" : "Log in"}
-          </Box>
-        </Flex>
-      </Box>
-    </>
-  );
-}
+			<Box border={"1px solid gray"} borderRadius={4} padding={5}>
+				<Flex alignItems={"center"} justifyContent={"center"}>
+					<Box mx={2} fontSize={14}>
+						{isLogin ? "Don't have an account?" : "Already have an account?"}
+					</Box>
+					<Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
+						{isLogin ? "Sign up" : "Log in"}
+					</Box>
+				</Flex>
+			</Box>
+		</>
+	);
+};
 
 export default AuthForm;
